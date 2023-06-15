6d3995c021b1d27c15cc58ca408d1b1465e8637f
leticea <nascimento.leticeah@gmail.com>
2023-06-15 12:37:54 -0300

    feat(script.js): creating js codes

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
+let currentStep = 0;
+const formSteps = document.querySelectorAll(".form-step");
 const form = document.querySelector("form");
 
+form.addEventListener("click", (e) => {
+  if (!e.target.matches("[data-action]")) {
+    return;
+  }
+
+  const actions = {
+    next() {
+      currentStep++;
+    },
+    prev() {
+      currentStep--;
+    },
+  };
+
+  const action = e.target.dataset.action;
+  actions[action]();
+  updateActiveStep();
+  updateProgressStep();
+});
+
 form.addEventListener("submit", (e) => {
-  e.preventDefault()
+  e.preventDefault();
 });
